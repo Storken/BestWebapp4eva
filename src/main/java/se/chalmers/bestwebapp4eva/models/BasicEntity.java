0c4d312006d05e45bbc21b52f416c4f9ab6a5268
simonjrp <simon.jrp@gmail.com>
2014-10-08 10:46:02 +0200

    Added Javadoc for BasicEntity

--- src/main/java/se/chalmers/bestwebapp4eva/models/BasicEntity.java
+++ src/main/java/se/chalmers/bestwebapp4eva/models/BasicEntity.java
@@ -8,7 +8,7 @@ import se.chalmers.bestwebapp4eva.persistence.AbstractEntity;
 
 /**
  *
- * @author simon
+ * Simple class representing a basic entity (with title, price, quantity and unit as attributes).
  */
 @Entity
 public class BasicEntity extends AbstractEntity{
@@ -18,7 +18,7 @@ public class BasicEntity extends AbstractEntity{
         pcs,
         l
     }
-     
+    
     @Column(nullable=false)
     private String title;
     
@@ -36,6 +36,13 @@ public class BasicEntity extends AbstractEntity{
         
     }
     
+    /**
+     * Constructor for BasicEntity.
+     * @param title Title of the basic entity.
+     * @param price Price of the basic entity.
+     * @param quantity Quantity of the basic entity.
+     * @param unit Unit of the basic entity, like kg, pcs and l.
+     */
     public BasicEntity(String title, double price, double quantity, Unit unit) {
         this.title = title;
         this.price = price;
@@ -43,6 +50,14 @@ public class BasicEntity extends AbstractEntity{
         this.unit = unit;
     }
     
+    /**
+     * Constructor for BasicEntity. Should normally not be used, id provided by database for new objects.
+     * @param id Id of the basic entity.
+     * @param title Title of the basic entity.
+     * @param price Price of the basic entity.
+     * @param quantity Quantity of the basic entity.
+     * @param unit Unit of the basic entity, like kg, pcs and l.
+     */
     public BasicEntity(long id, String title, double price, double quantity, Unit unit) {
         super(id);
         this.title = title;
@@ -51,31 +66,58 @@ public class BasicEntity extends AbstractEntity{
         this.unit = unit;
     }
   
-    
+    /**
+     * Get the title.
+     * @return The title.
+     */
     public String getTitle() {
         return title;
     }
     
+    /**
+     * Get the quantity.
+     * @return The quantity.
+     */
     public double getQuantity() {
         return quantity;
     }
     
+    /**
+     * Get the price.
+     * @return The price.
+     */
     public double getPrice() {
         return price;
     }
     
+    /**
+     * Get the unit (price per *unit* and/or quantity *unit*). 
+     * @return The unit.
+     */
     public Unit getUnit() {
         return this.unit;
     }
     
+    /**
+     * Set the title.
+     * @param title The new title.
+     */
     public void setTitle(String title) {
         this.title = title;
     }
     
+    /**
+     * Set the quantity.
+     * @param quantity The new quantity.
+     */
     public void setQuantity(double quantity) {
         this.quantity = quantity;
     }
     
+    /**
+     * Set the price.
+     * @param price The new price.
+     */
     public void setPrice(double price) {
         this.price = price;
     }
