1714c8e6c912a739d939fa0d727d23b333e65f2a
simonjrp <simon.jrp@gmail.com>
2014-10-08 10:19:29 +0200

    Added IMainContainer.java and MainContainer.java

---
+++ src/main/java/se/chalmers/bestwebapp4eva/models/IMainContainer.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.chalmers.bestwebapp4eva.models;
+
+/**
+ *
+ * @author simon
+ */
+public interface IMainContainer {
+    
+    public IBasicEntityCollection getBasicEntityCollection();
+}
