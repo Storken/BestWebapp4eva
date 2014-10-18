c92cb9cf4f67f19306ee252149734fb17e1a195e
simonjrp <simon.jrp@gmail.com>
2014-10-18 18:26:30 +0200

    Added CategoryCollection DAO and accompanying interface

---
+++ src/main/java/se/chalmers/bestwebapp4eva/dao/ICategoryCollection.java
@@ -0,0 +1,13 @@
+package se.chalmers.bestwebapp4eva.dao;
+
+import java.util.List;
+import se.chalmers.bestwebapp4eva.entity.Category;
+
+/**
+ *
+ * @author simon
+ */
+public interface ICategoryCollection extends IDAO<Category, Long> {
+    
+    public List<Category> getByName(String name);
+}
