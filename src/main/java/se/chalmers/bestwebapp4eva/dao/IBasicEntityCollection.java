169fbfba1f16cc157f465560e76a09895fac2242
simonjrp <simon.jrp@gmail.com>
2014-10-18 18:00:16 +0200

    Moved method getResultList from IDAO to IBasicEntityCollection

--- src/main/java/se/chalmers/bestwebapp4eva/dao/IBasicEntityCollection.java
+++ src/main/java/se/chalmers/bestwebapp4eva/dao/IBasicEntityCollection.java
@@ -1,7 +1,9 @@
 package se.chalmers.bestwebapp4eva.dao;
 
 import java.util.List;
+import java.util.Map;
 import javax.ejb.Local;
+import org.primefaces.model.SortOrder;
 import se.chalmers.bestwebapp4eva.entity.BasicEntity.Unit;
 import se.chalmers.bestwebapp4eva.entity.BasicEntity;
 import se.chalmers.bestwebapp4eva.entity.Category;
@@ -49,6 +51,22 @@ public interface IBasicEntityCollection extends IDAO<BasicEntity, Long> {
     public List<BasicEntity> getByUnit(Unit unit);
     
     public List<BasicEntity> getByCategory(Category category);
+    
+    /**
+     * Method for getting a specific list of entities from the db. The list
+     * might be sorted and filtered.
+     *
+     * @param first The position of the first result to retrieve.
+     * @param pageSize Maximum number of results to retrieve.
+     * @param sortField Which field the list should be sorted by.
+     * @param sortOrder In which order the list should be sorted
+     * (SortOrder.ASCENDING, SortOrder.DECENDING etc.).
+     * @param filters A map with filters. The keys are the names of the
+     * attributes and the values are the wanted values of the attributes.
+     * @return A list of results from the db that's sorted and filtered
+     * according to the parameters.
+     */
+    public List<BasicEntity> getResultList(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, Object> filters);
 
     /**
      * Temporary method for testing purposes only. To be removed.
--- src/main/java/se/chalmers/bestwebapp4eva/dao/IDAO.java
+++ src/main/java/se/chalmers/bestwebapp4eva/dao/IDAO.java
@@ -29,20 +29,4 @@ public interface IDAO<T, K> {
 
     public int count(String sortField, SortOrder sortOrder, Map<String, Object> filters);
 
-    /**
-     * Method for getting a specific list of entities from the db. The list
-     * might be sorted and filtered.
-     *
-     * @param first The position of the first result to retrieve.
-     * @param pageSize Maximum number of results to retrieve.
-     * @param sortField Which field the list should be sorted by.
-     * @param sortOrder In which order the list should be sorted
-     * (SortOrder.ASCENDING, SortOrder.DECENDING etc.).
-     * @param filters A map with filters. The keys are the names of the
-     * attributes and the values are the wanted values of the attributes.
-     * @return A list of results from the db that's sorted and filtered
-     * according to the parameters.
-     */
-    public List<T> getResultList(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, Object> filters);
-
 }
